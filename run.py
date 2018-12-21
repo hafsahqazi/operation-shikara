5ab7579a98f91b6010c78f2ce1b6893822d9b050
Hafsah Qazi <hafsah.qazi@gmail.com>
2018-12-21 18:49:06 -0500

    initial commit

--- run.py
+++ run.py
@@ -1,11 +1,15 @@
-from flask import Flask
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    dictionary = {
+        'Hafsah': 'Stupid',
+        'Musab': 'Genius'
+    }
+    return jsonify(dictionary)
 
 
 app.run(host="0.0.0.0", use_reloader=False)
