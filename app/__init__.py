ad4a0168215bdb9da34dd5105bd908014d78f37c
Qazi Musab <musab.ahmed@gmail.com>
2018-12-21 18:32:25 -0500

    Structured code

--- app/__init__.py
+++ app/__init__.py
@@ -1,3 +1,16 @@
+import os
+
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
+
+app = Flask(__name__)
+db = SQLAlchemy(app)
+cors = CORS(app)
 
 
-
+def create_app():
+    app.config.from_object(os.environ['APP_SETTINGS'])
+    app.config['CORS_HEADERS'] = 'Content-Type'
+    db.init_app(app)
+    return app
---
+++ config.py
@@ -0,0 +1,40 @@
+import os
+
+from app import app
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+
+class Config(object):
+    DEVELOPMENT = False
+    DEBUG = False
+    TESTING = False
+    LOCAL = True
+    CSRF_ENABLED = True
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'postgres://postgres@localhost/hafsahqazi')
+
+
+class ProductionConfig(Config):
+    DEVELOPMENT = False
+    DEBUG = False
+    TESTING = False
+    LOCAL = False
+    SAVE_LOCATION_INFO_FROM_IP = True
+
+
+class StageConfig(Config):
+    DEVELOPMENT = True
+    DEBUG = True
+    LOCAL = False
+
+
+class DevelopmentConfig(Config):
+    DEVELOPMENT = True
+    DEBUG = True
+    LOCAL = True
+
+
+class DemoConfig(Config):
+    DEVELOPMENT = True
+    DEBUG = True
+    LOCAL = False
